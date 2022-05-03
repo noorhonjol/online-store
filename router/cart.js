9bdef67ef363fe148c1a0d9e9a3dafd04f85744e
noorhonjol <nooralhongal@gmail.com>
2022-05-03 19:09:52 +0300

    edit some redirect

--- router/cart.js
+++ router/cart.js
@@ -26,7 +26,6 @@ router.post('/products/category/shop',async(req,res)=>{
 router.get('/cart',async(req,res)=>{
 
     const [rows] = await db.pool.query(`SELECT pName,pPrice,image,count FROM product ,cart WHERE cart.proID=product.proID AND id =${req.session.u_id};`);
-        console.log(rows)
     res.render('cart',{cart:rows})
 
 })
