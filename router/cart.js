43bae42c42374805f3beab324f344f7bd8826003
yousefQJ19 <yousefnajeh3@gmail.com>
2022-05-07 04:29:10 +0300

    favorite page part-1

--- router/cart.js
+++ router/cart.js
@@ -4,24 +4,38 @@ const db=require('../models/db');
 
 
 router.post('/products/category/shop',async(req,res)=>{
-    const {pID}=req.body
-    if(req.session.isLogin){
-        
-        const [rows]=await db.pool.query(`SELECT * FROM cart WHERE id=${req.session.u_id} AND proID=${pID}`);
-        
-        if(!rows.length){
-            await db.pool.query(`INSERT INTO cart VALUES (${req.session.u_id},${pID},1,1,1,1);`);
-        }
-        else{
-            await db.pool.query(`UPDATE cart SET count =count+1 WHERE id = ${req.session.u_id} AND proID = ${pID} `)
+    const {fpID,cpID}=req.body
+    if(fpID){
+        if(req.session.isLogin){
+
+            const [rows]=await db.pool.query(`SELECT * FROM fav WHERE id=${req.session.u_id} AND proID=${fpID}`);
             
+            if(!rows.length){
+                await db.pool.query(`INSERT INTO fav VALUES (${req.session.u_id},${fpID});`);
+            }
+        }else{
+            res.redirect('/login')
         }
-    }else{
-        res.redirect('/login')
+    }
+    else{
+        if(req.session.isLogin){
+            
+            const [rows]=await db.pool.query(`SELECT * FROM cart WHERE id=${req.session.u_id} AND proID=${cpID}`);
+            
+            if(!rows.length){
+                await db.pool.query(`INSERT INTO cart VALUES (${req.session.u_id},${cpID},1,1,1,1);`);
+            }
+            else{
+                await db.pool.query(`UPDATE cart SET count =count+1 WHERE id = ${req.session.u_id} AND proID = ${cpID} `)
+                
+            }
+        }else{
+            res.redirect('/login')
+        }
     }
 
-    res.status(204).send();
-
+        res.status(204).send();
+        
 })
 router.get('/cart',async(req,res)=>{
 
@@ -29,6 +43,12 @@ router.get('/cart',async(req,res)=>{
     res.render('cart',{cart:rows,session:req.session})
 
 })
+router.get('/favorite',async(req,res)=>{
 
+    const [rows] = await db.pool.query(`SELECT pName,pPrice,image FROM product ,fav WHERE fav.proID=product.proID AND id =${req.session.u_id};`);
+        console.log(rows)
+    // res.render('cart',{cart:rows,session:req.session})
+
+})
 
 module.exports=router
